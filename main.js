90618db38863ce9d90cc3ab299ccf254828707b6
DondiLunsila <135969158+DondiLunsila@users.noreply.github.com>
2023-08-07 10:09:12 +0000

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // The keys and notes variables store the piano keys
-// Le debut
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
 keys.forEach(function(key){
